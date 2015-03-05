8b6f5d4af192dd0fe0cf8153a8ec2e0fdd6d9627
Bhargava Manja <bhargee.manja@gmail.com>
2015-03-04 20:28:33 -0600

    Changed for UDP, weird error in JNI stuff

--- app/src/main/java/com/research/bretl/robovidd/MainActivity.java
+++ app/src/main/java/com/research/bretl/robovidd/MainActivity.java
@@ -10,18 +10,17 @@ import android.widget.ImageView;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.highgui.Highgui;
 
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 public class MainActivity extends ActionBarActivity {
-    private ServerSocket server_sock;
     private static final int PORT = 3490;
-    private static final int IMGSIZE = 921600;
+    private static final int MAX_UDP_DATAGRAM_LEN = 64000;
     private ImageView im;
     private Bitmap bm;
 
@@ -30,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (!OpenCVLoader.initDebug()) {
-            Log.d("rbv","Loading OpenCV failed, the app will crash");
+            Log.d("rbv","Loading OpenCV shared libs failed, the app will crash");
         }
         im = (ImageView) findViewById(R.id.image_view);
         Thread server_th = new Thread(new ServerThread());
@@ -58,19 +57,20 @@ public class MainActivity extends ActionBarActivity {
     }
 
     class ServerThread implements Runnable {
-        private Socket client_sock;
+        byte[] frame = new byte[MAX_UDP_DATAGRAM_LEN];
+        DatagramPacket packet = new DatagramPacket(frame, frame.length);
+        DatagramSocket socket = null;
         public void run() {
             try {
-                server_sock = new ServerSocket(PORT);
-
+                socket = new DatagramSocket(PORT);
             } catch(IOException e) {
                 e.printStackTrace();
             }
             while (!Thread.currentThread().isInterrupted()) {
-                Log.d("rbv", "loopin loopin loopin");
                 try {
-                    client_sock = server_sock.accept();
-                    Thread ui_update = new Thread(new UIUpdateThread(client_sock));
+                    socket.receive(packet);
+                    //Log.d("rbv", "frame size in bytes - " + packet.getLength());
+                    Thread ui_update = new Thread(new UIUpdateThread(packet));
                     ui_update.start();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -80,39 +80,22 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private class UIUpdateThread implements Runnable {
-        private Socket client_sock;
-        public UIUpdateThread (Socket client_sock) {
-            this.client_sock = client_sock;
+        private DatagramPacket packet;
+        public UIUpdateThread (DatagramPacket packet) {
+            this.packet = packet;
         }
         public void run() {
             while (!Thread.currentThread().isInterrupted()) {
-                int times = 0;
-                byte[] frame_bytes = new byte[IMGSIZE];
-                Mat frame = Mat.zeros(480, 640, CvType.CV_8UC3);
-                try {
-                    DataInputStream input = new DataInputStream(client_sock.getInputStream());
-                    input.readFully(frame_bytes, 0, IMGSIZE);
-                    Log.d("rbv", "received " + (++times) + " image(s)");
-                    input = new DataInputStream(client_sock.getInputStream());
-                    input.readFully(frame_bytes, 0, IMGSIZE);
-                    int ptr = 0;
-                    for (int i = 0; i < frame.rows(); i++) {
-                        for (int j = 0; j < frame.cols(); j++) {
-                            frame.put(i, j, new byte[]{frame_bytes[ptr + 2], frame_bytes[ptr + 1], frame_bytes[ptr]});
-                            ptr += 3;
-                        }
+                Mat frame = Highgui.imdecode(new MatOfByte(packet.getData()), 0);
+                bm = Bitmap.createBitmap(frame.cols(), frame.rows(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(frame, bm);
+                im.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        im.setImageBitmap(bm);
                     }
-                    bm = Bitmap.createBitmap(frame.cols(), frame.rows(), Bitmap.Config.ARGB_8888);
-                    Utils.matToBitmap(frame, bm);
-                    im.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            im.setImageBitmap(bm);
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                });
+
             }
         }
     }
